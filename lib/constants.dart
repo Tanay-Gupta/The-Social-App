73d5e77c39f06f45cdae20a757e846a59c783729
Tanay-Gupta <tanaygupta811@gmail.com>
2022-05-29 16:04:46 +0530

    added heading style

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:swag_app/size_config.dart';
 
 const kPrimaryColor = Color(0xffCD0000);
 const kTextColor = Color(0xFFFFFFFF);
+
+final headingStyle = TextStyle(
+  fontSize: getProportionateScreenWidth(28),
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
+  height: 1.5,
+);
